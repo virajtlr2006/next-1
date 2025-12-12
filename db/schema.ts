c3b2d7b69856c52946f43c26d120231fb2f9547d
virajtlr2006 <virajtlr2006@gmail.com>
2025-12-12 06:32:31 +0530

    Feat(db for service):Schema setup done

---
+++ db/schema.ts
@@ -0,0 +1,14 @@
+import { $Type } from "drizzle-orm";
+import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
+
+export const serviceTable = pgTable("service", {
+  service_id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  email:varchar().notNull(),
+  service_name:varchar().notNull(),
+  desc:varchar().notNull(),
+  price:integer().notNull(),
+  category:varchar().notNull(),
+});
+
+export type service = typeof serviceTable.$inferSelect;
+export type newService = typeof serviceTable.$inferInsert;
